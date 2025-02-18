821871db89861fc9fd86099d5197d1a69d03d160
12ArtuR21 <artur.nuriev134@gmail.com>
2025-02-18 18:03:45 +0400

    Add files via upload

---
+++ 1 лабораторная.py
@@ -0,0 +1,21 @@
+# Четные четырехричные числа, не превышающие 204710, у которых вторая справа цифра равна 1. Выводит на экран цифры числа, исключая единицы. Вычисляется среднее число между минимальным и максимальным и выводится прописью.
+with open("chisla.txt", "w") as fd:
+    for element in range (1000, 2048):
+        fd.write(str(element) + '\n')
+
+with open('chisla.txt') as fd:
+    file = fd.read().split()
+print(*file)
+
+WORDS_ONES = ['ноль', 'один', 'два', 'три', 'четыре', 'пять', 'шесть', 'семь', 'восемь', 'девять']
+
+def num_to_word(sr_num):
+        return WORDS_ONES[sr_num]
+
+for element in file:
+    string_element = str(element)
+    if int(element) % 2 == 0 and string_element[2] == '1':
+        spisok = [string_element[0], string_element[1], string_element[3]]
+        spisok.sort()
+        sr_num = (int(spisok[0]) + int(spisok[2])) // 2
+        print('Цифры числа: ', *spisok, '.', ' Среднее число между минимальным и максимальным: ', sr_num, ' (', num_to_word(sr_num), ')', sep="")
